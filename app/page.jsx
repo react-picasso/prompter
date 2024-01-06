3454f016cd2f1ae768235d42b7e5a54a9e01cf53
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2024-01-06 12:43:12 +0100

    Initial commit | Created homepage

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+	return (
+		<section className="w-full flex-center flex-col">
+			<h1 className="head_text text-center">
+				Discover & Share
+                <br className="max-md:hidden" />
+                <span className="blue_gradient text-center">AI-powered prompts</span>
+			</h1>
+            <p className="desc text-center">
+                Prompter is an open-source project that aims to provide a platform for users to discover and share AI-generated prompts.
+            </p>
+		</section>
+	);
+};
+
+export default Home;
