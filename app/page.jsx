be4fb7220b3d221034204dbad6576ed58ccb4b6d
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2024-01-06 14:25:44 +0100

    feat: implemented auth

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,10 @@ const Home = () => {
 	return (
 		<section className="w-full flex-center flex-col">
 			<h1 className="head_text text-center">
-				Discover & Share
+				Discover & Share 
                 <br className="max-md:hidden" />
-                <span className="blue_gradient text-center">AI-powered prompts</span>
+
+                <span className="blue_gradient text-center"> AI-powered Prompts</span>
 			</h1>
             <p className="desc text-center">
                 Prompter is an open-source project that aims to provide a platform for users to discover and share AI-generated prompts.
